4db5d42887fe7c1e2ce849a177c4126946b38706
JoeBlakey <joeblakey@outlook.com>
2023-07-22 14:38:32 +0100

    Fully reworked solution

--- Budget_App/budget.py
+++ Budget_App/budget.py
@@ -1,103 +1,108 @@
-class Category():
+class Category:
+  
+  def __init__(self, categories):
+    self.categories = categories
+    self.ledger = []
+    self.spent = 0
+    self.percentage_spent = 0
+
+  def __str__(self):
+    categories_length = len(self.categories)
+    output = ""
     
-    def __init__(self, name):
-        self.ledger = []
-        self.current_balance = 0
-        self.deposited = 0
-        self.spent = 0
-        self.pos_funds = True
-        self.name = name
+    #First Line
+    for x in range(0, int((30-categories_length)/2)):
+      output += "*"
+    if (categories_length % 2) != 0:
+      output += "*"
+    output += self.categories
+    for x in range(0, int((30-categories_length)/2)):
+      output += "*"
+    output += "\n"
 
-    def __repr__(self):
-        ledger = ""
-        total = 0
-        title = f'{self.name}'
-        title_line = title.center(30, '*')
-        for n in self.ledger:
-            line_desc = "{:<23}".format(n["description"])
-            line_amount = "{:>7.2f}".format(float(n["amount"]))
-            ledger = ledger + f"{line_desc[:23]}{line_amount:<7}" + "\n"
-            total = total + float(n["amount"])
-        total = str(total)
-        cat_output = title_line + "\n" + ledger + "Total: " + total
-        return cat_output
+    #Description + Amount
+    line = []
+    for x in self.ledger:
+      line = x['description'][0:23]
+      for y in range(len(line), 23):
+        line += " "
 
-    def deposit(self, amount, desc=False):
-        if desc is False:
-            desc = ""
-        depo = {"amount": f'{amount}', "description": f'{desc}'}
-        self.ledger.append(depo)
-        self.current_balance = self.current_balance + amount
-        self.deposited = self.deposited + amount
-        print(self.ledger)
+      amt = str("{:.2f}".format(x['amount']))   
+      for y in range(0, 7 % len(amt)):
+        line += " "
+      line += amt + "\n"
+      output += line
+  
+    #Category Total Balance
+    output += "Total: " + str(self.get_balance())
     
-    def withdraw(self, amount, desc=False):
-        if desc is False:
-            desc = ""
-        withd = {"amount": f'{-amount}', "description": f'{desc}'}
-        self.check_funds(amount)
-        if self.pos_funds:
-            self.current_balance = self.current_balance - amount
-            self.spent = self.spent + amount
-            self.ledger.append(withd)
-            print(self.ledger)
-            return True
-        else:
-            return False
-    
-    def get_balance(self):
-        return self.current_balance
-    
-    def transfer(self, amount, new_cat):
-        # Needs destination adding
-        if (self.withdraw(amount, f"Transfer to {new_cat.name}")):
-            print("Transfer Successful")
-            new_cat.deposit(amount, f"Transfer from {self.name}")
-            self.spent = self.spent + amount
-            self.deposited = self.deposited + amount
-            return True
-        else:
-            print("Transfer Failed")
-            return False        
-    
-    def check_funds(self, amount):
-        if amount > self.current_balance:
-            print("No")
-            self.pos_funds = False
-            return False
-        else:
-            print("Yes")
-            self.pos_funds = True
-            return True 
+    return output
 
-def create_spend_chart(self):
-    n = 0
-    num_args = len(self)
-    while n < num_args:
-        percentage = (self[n].spent / self[n].deposited) * 100
-        rounded_perc = round(percentage/10)*10
-        n = n + 1
-    title = "Percentage Spent by Category\n"
+  def deposit(self, amount, description = ""):
+    self.ledger.append({"amount": amount, "description": description})
 
-    chart = ""
-    for value in reversed(range(0,101,10)):
-        chart += str(value).rjust(3)+"|"
-        if rounded_perc >= value:
-            chart += " o "
-        else:
-            chart += "   "
-        chart += "\n"
+  def withdraw(self, amount, description = ""):
+    if self.check_funds(amount):
+      self.ledger.append({"amount": -amount, "description": description})
+      self.spent += -amount
+      return True
+    else:
+      return False
+  
+  def get_balance(self):
+    balance = 0
+    for x in self.ledger:
+      balance += x['amount'] 
+    return balance
 
-    print(chart)
+  def transfer(self, amount, new_cat):
+    if self.withdraw(amount, "Transfer to " + new_cat.categories):
+      new_cat.deposit(amount, "Transfer from " + self.categories)
+      return True
+    else:
+      return False
+  
+  def check_funds(self, amount):
+    if amount <= self.get_balance():
+      return True
+    else:
+      return False
+  
+def create_spend_chart(categories):
+  
+  total_spent = 0
+  for category in categories:
+    total_spent += category.spent
 
-food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-print(clothing.get_balance())
-print(food)
-print(clothing)
-create_spend_chart([food, clothing])
+  for category in categories:
+    category.percentage_spent = int(category.spent*100/total_spent) 
+
+  output = "Percentage spent by category\n"
+  for x in range (100, -1, -10):
+    output += str(x).rjust(3) + "| "
+    for y in categories:
+      if y.percentage_spent >= x:
+        output += "o  "
+      else:
+        output += "   "
+    output += "\n"
+
+  output += "    -"
+  for x in range(0, len(categories)):
+    output += "---"
+  output += "\n"
+
+  #List Comprehension - max len of categories name
+  max_len_categories_name = max([len(category.categories) for category in categories])
+  
+  for x in range(0, max_len_categories_name):
+    output += "     "
+    for category in categories:
+      if x < len(category.categories):
+        output += category.categories[x] + "  "
+      else:
+        output += "   "
+    if x != max_len_categories_name - 1:
+      output += "\n"
+
+  return output
